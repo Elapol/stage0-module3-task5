3f6201cc34af6cfd889865029f95539cc6f425e2
elyad <ella.dikovich@gmail.com>
2022-10-28 16:23:40 +0200

    done111

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,8 +1,7 @@
 package lang.print.gaps.task5;
 public class TimeConvertor {
     public static float convert(float minutes) {
-                minutes = minutes * 60;
-                return minutes;
+                return minutes * 60;
             }
             public static void main(String[] args) {
                 System.out.println(convert(1));
