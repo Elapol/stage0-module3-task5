8177a729517378a05982e9a163685c5537b2f609
elyad <ella.dikovich@gmail.com>
2022-10-29 15:36:40 +0200

    katiaaa

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,14 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-
-    static int divide(int toBeDivided) {
-        toBeDivided = toBeDivided / 5;
-        return toBeDivided;
-    }
-
     public static void main(String[] args) {
-        System.out.println(divide(5));
-        System.out.println(divide(25));
+        divide(30);
     }
-}
+        public static void divide(int toBeDivided) {
+        int result = toBeDivided / 5;
+        System.out.println(result);
+        }
+    }
+
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,12 +1,11 @@
 package lang.print.gaps.task5;
 public class TimeConvertor {
-    public static float convert(float minutes) {
+    public static void main(String[] args) {
+        convert(4);
+    }
+    public static void convert(float minutes) {
         float sec = minutes *60;
-                return sec;
-            }
-            public static void main(String[] args) {
-                System.out.println(convert(1));
-                System.out.println(convert(5));
+        System.out.println(sec);
 
             }
         }
